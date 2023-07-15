f795d93ad300618d30324f2d4f863cc80301c025
owen <owen_zty@foxmail.com>
2023-07-15 14:37:15 +0800

    Basic Authorization verify

--- ssr3.py
+++ ssr3.py
@@ -6,7 +6,6 @@ password = 'admin'
 
 user_info_str = username + ':' + password
 user_info = base64.b64encode(user_info_str.encode())
-
 url = 'https://ssr3.scrape.center'
 headers ={
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
@@ -14,4 +13,3 @@ headers ={
 }
 
 resp = requests.get(url, headers=headers)
-print(resp.text)
